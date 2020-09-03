bf00bf91ae11726cf259a7e8857cb79372d59852
Grigorie Vlad Ioan <vlad_grigorie@yahoo.com>
2020-09-03 15:18:47 +0300

    Tuples

---
+++ tuples.py
@@ -0,0 +1,22 @@
+#definition
+t=(1, 2, 3, 1)
+t2=('one', 2)
+print(t)
+print(t2)
+
+#count repeated elements
+print(t.count(1))
+
+#get index of element
+print(t.index(3))
+    #if duplicates - it retuns the first one
+
+#compare with list
+my_list = [1, 2, 3]
+type(t)
+type(my_list)
+print(t[0])
+
+#tuples dont support item assigment : 
+#   t[0] = "NEW" => error
+
