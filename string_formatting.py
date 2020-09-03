a587a555c68b6d86fe9b58b7afc84b171b9c1aa2
Grigorie Vlad Ioan <vlad_grigorie@yahoo.com>
2020-09-03 14:56:53 +0300

    Modified string_formatting.py

--- string_formatting.py
+++ string_formatting.py
@@ -1,18 +1,19 @@
-# FORMAT :
-# basic
+#Format - basic
 print("\nThis is a string {}".format("INSERTED")) 
-# index
+
+#index
 print("\nThe {2} {1} {0}".format("fox", "brown", "quick")) 
-# keywords
+
+#keywords
 print("\nThe {f} {b} {q}".format(f="fox", b="brown", q ="quick")) 
-# number formatting
+
+#number formatting
 result = 100/777
 print("\nThe result was {r:1.3f}".format(r=result))
     # 1 - width (white space)
     # 3 - precision - numbers past decimal point
 
-# Formatted string literals (since python 3.6):
-
+#Formatted string literals (since python 3.6):
 name = "Jose"
 print(f'Hello, his name is {name}')
     # the f is mandatory
