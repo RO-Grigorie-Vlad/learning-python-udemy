8121b1db3e585cd040d36ae461af0749302c103b
Grigorie Vlad Ioan <vlad_grigorie@yahoo.com>
2020-09-03 15:35:38 +0300

    Boolean and None types

---
+++ section3/booleans_none.py
@@ -0,0 +1,15 @@
+#definition - you know what this is
+#has to have capital letter T, F
+a = True
+b = False
+print(True)
+print(a)
+print(b)
+
+#check type
+print(type(b))
+print(type(False))
+
+#None type
+b = None # null from Java
+print(b)
